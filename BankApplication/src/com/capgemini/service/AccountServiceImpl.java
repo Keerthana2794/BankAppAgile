e1ca3ddb2c481ee7df31476fb821f3c69c712210
lnc <lnc>
2017-03-15 11:02:49 +0530

    Bank APllication

---
+++ BankApplication/src/com/capgemini/service/AccountServiceImpl.java
@@ -0,0 +1,65 @@
+package com.capgemini.service;
+
+import com.capgemini.beans.Account;
+import com.capgemini.exceptions.InsufficientBalanceException;
+import com.capgemini.exceptions.InsufficientInitalAmountException;
+import com.capgemini.exceptions.InvalidAccountNumberException;
+import com.capgemini.repository.AccountRepository;
+
+public class AccountServiceImpl implements AccountService {
+	
+	AccountRepository accountRepository;
+	
+	public AccountServiceImpl(AccountRepository accountRepository)
+	{
+		this.accountRepository=accountRepository;
+	}
+	
+	/* (non-Javadoc)
+	 * @see com.capgemini.service.AccountService#createAccount(int, int)
+	 */
+	@Override
+	public Account createAccount(int accountNumber,int amount)throws InsufficientInitalAmountException
+	{
+		if(amount<500)
+		{
+			throw new InsufficientInitalAmountException();
+		}
+		
+		Account account =new Account();
+		account.setAccountNumber(accountNumber);
+		account.setAmount(amount);
+		
+		if(accountRepository.save(account))
+		{
+			return account;
+		}
+		
+		
+		return null;
+	}
+	
+	/* (non-Javadoc)
+	 * @see com.capgemini.service.AccountService#depositAmount(int, int)
+	 */
+	@Override
+	public int depositAmount(int accountNumber,int amount)throws InvalidAccountNumberException
+	{
+		if(accountNumber<100 || accountNumber>10000)
+		{
+			throw new InvalidAccountNumberException();
+		}
+		return 0;
+	}
+	
+	/* (non-Javadoc)
+	 * @see com.capgemini.service.AccountService#withdrawAmount(int, int)
+	 */
+	@Override
+	public int withdrawAmount(int accountNumber,int amount)throws InvalidAccountNumberException,InsufficientBalanceException
+	{
+		return 0;
+	}
+	
+
+}
